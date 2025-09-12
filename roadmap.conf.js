40d8874461bc0b393eae06b0859aa0a9e6db2730
Konstantinos Kaloutas <sprocketc@gmail.com>
2025-09-12 13:23:49 +0300

    reintroduce roadmap

---
+++ roadmap.conf.js
@@ -0,0 +1,25 @@
+'use strict'
+
+module.exports = {
+  // Name of the organization or project this roadmap is generated for
+  organization: 'Repath Studio',
+
+  // Include open and closed milestones where due date is after milestonesStartDate
+  milestonesStartDate: '2020-01-01T00:00:00Z', // ISO formatted timestamp
+
+  // Include open and closed milestones where due date is before milestonesEndDate
+  milestonesEndDate: '2026-11-01T00:00:00Z', // ISO formatted timestamp
+
+  // Github repository to open a Pull Request with the generated roadmap
+  targetRepo: "repath-project/repath-studio",
+
+  // List of projects that this roadmap covers
+  projects: [
+    {
+      name: "Repath Studio",
+      repos: [
+        "repath-project/repath-studio",
+      ],
+    },
+  ]
+}
